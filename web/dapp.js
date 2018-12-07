879909ddc1d1e1803adcd35d89530840861d3b01
bao.duong@tech.reapra.sg <duonghuynhbao@Duongs-MacBook-Pro.local>
2018-12-07 16:03:31 +0700

    edit GUI

--- web/dapp.js
+++ web/dapp.js
@@ -1,3 +1,53 @@
+const server = "http://127.0.0.1:3000";
+const signed_msg="HealthcareFHIR Signature";
+function on_org_fhir_management(){
+    //alert('on_org_fhir_management');
+    eth_personal_sign
+}
+function on_patients_fhir_management(){
+    alert('on_patients_fhir_management');
+}
+async function on_page_load(){
+    if (window.ethereum) {// Modern dapp browsers...
+        window.web3 = new Web3(ethereum);
+        try {
+            await ethereum.enable();  //  // Request account access if needed.Acccounts now exposed     
+        } catch (error) {
+            console.log(error);
+        }
+    } else if (window.web3) {//  Legacy dapp browsers...
+        window.web3 = new Web3(web3.currentProvider);
+    } else {  // Non-dapp browsers...
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+}
+
+function setHTMLTag(tagId, info_str) {
+    document.getElementById(tagId).innerHTML = info_str;
+}
+function getHTMLTag(tagId) {
+    return document.getElementById(tagId).value;
+}
+function connect() {
+    if (typeof ethereum !== 'undefined') {
+        ethereum.enable()
+            .catch(console.error)
+    }
+}
+function eth_personal_sign(str_msg) {
+    let from = get_selected_addr();
+    if (!from) return connect();
+    return web3.eth.getBlockNumber()
+        .then((blockcnt) => {
+            var nonce = Math.floor(blockcnt / 10);
+            str_msg = str_msg + ':' + nonce;
+            return web3.eth.personal.sign(str_msg, from, "");//sign through metamask
+        })
+}
+function get_selected_addr() {
+    return web3.currentProvider.selectedAddress;
+}
+
 function setup_smartcontract(){
     window._gasLimit = 1500000;
     window._gasPrice = '20000000000';
@@ -9,11 +59,11 @@ function setup_smartcontract(){
     return window.contract;
 }
 
+//================Smartcontract Handle========================
 function getFee(){
     let _fromaddr = web3.currentProvider.selectedAddress;
     return contract.methods.getFee().call({ from: _fromaddr });
 }
-
 function updateOrgRegisterInfo(org_info,fee_wei){
     let _fromaddr = web3.currentProvider.selectedAddress;
     let opt = {
@@ -24,7 +74,6 @@ function updateOrgRegisterInfo(org_info,fee_wei){
     }
     return contract.methods.updateOrgRegisterInfo(org_info).send(opt);
 }
-
 function getOrgName(_orgId){
     let _fromaddr = web3.currentProvider.selectedAddress;
     return contract.methods.getOrgName(_orgId).call({ from: _fromaddr });
--- web/org.js
+++ web/org.js
@@ -1,35 +1,10 @@
-const server = "http://127.0.0.1:3000";
-window.addEventListener('load', on_page_load);
-function setHTMLTag(tagId, info_str) {
-    document.getElementById(tagId).innerHTML = info_str;
-}
-function getHTMLTag(tagId) {
-    return document.getElementById(tagId).value;
-}
-function connect() {
-    if (typeof ethereum !== 'undefined') {
-        ethereum.enable()
-            .catch(console.error)
-    }
-}
-async function on_page_load() {
-    if (window.ethereum) {// Modern dapp browsers...
-        window.web3 = new Web3(ethereum);
-        try {
-            await ethereum.enable();  //  // Request account access if needed.Acccounts now exposed     
-        } catch (error) {
-            console.log(error);
-        }
-    } else if (window.web3) {//  Legacy dapp browsers...
-        window.web3 = new Web3(web3.currentProvider);
-        // Acccounts always exposed
-    } else {  // Non-dapp browsers...
-        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-        //setHTMLTag('txt_status','Can not find Metamask. Please Install Metamask plugin')
-    }
+
+window.addEventListener('load', async()=>{
+    await on_page_load();
     setup_smartcontract();
     display_info();
-}
+});
+
 function display_info(){
     let _from =web3.currentProvider.selectedAddress;
     setHTMLTag('txt_web3api', web3.version);
